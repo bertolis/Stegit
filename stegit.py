3cb57a952ef5400f8ca0ed28b87be3f99d6f35f8
bertolis24 <grigoris@localhost.localdomain>
2019-06-14 11:21:55 +0300

    First commit

---
+++ stegit.py
@@ -0,0 +1,278 @@
+from PIL import Image
+from cryptography.fernet import Fernet
+import math
+import os
+import re
+import binascii
+import itertools
+import base64
+
+
+def encryption():
+    word = input("Give message to encrypt: ")
+
+    key = Fernet.generate_key()
+    cipher_suite = Fernet(key)
+    cipher_text = cipher_suite.encrypt(bytes(word, encoding='utf-8'))
+
+    print("Key: " + str(key) + "\n")
+    print(cipher_text)
+
+
+def decryption():
+    word = input("Give message to encrypt: \n")
+    key = input("\nGive key to encrypt with: \n")
+
+    cipher_suite = Fernet(key)
+    plain_text = cipher_suite.decrypt(bytes(word, encoding='utf-8'))
+
+    print("\n"+str(plain_text.decode("utf-8")))
+
+
+def brute_force(messages):
+    message_perm = ""
+    text = ""
+
+    """
+    for subset in itertools.permutations(messages):
+        for i in subset:
+            text+=i
+        text+='\n'
+
+    basetext=text.split('\n')
+
+    for i in basetext:
+        try:
+            #print((base64.b64decode(i)).decode("utf-8"))
+            message_comb += (base64.b64decode(i)).decode("utf-8")
+            message_comb += "\n"
+        except:
+            pass
+            #print("Non printable")
+   
+    message_comb = message_comb.split('\n') 
+    for m in message_comb[:20]:
+        print(m)
+    """
+
+    for mess in messages:
+        i = 0
+        if "TheKeyForTheDecryptionIs" in mess:
+            text = mess[24:]
+            cipher_suite = Fernet(mess[24:])
+            messages.remove(mess)
+            break
+        i += 1
+
+    #print(text)
+    #print(messages)
+
+    # Searching all the possible combinations
+    for permutation in itertools.permutations(messages):
+        #print (combination)
+        for perm in permutation:
+            message_perm += perm
+        try:
+            plain_text = cipher_suite.decrypt(bytes(message_perm, encoding='utf-8'))
+            print("\nThe hidden message is:\n\n"+str(plain_text.decode("utf-8")))
+            break
+        except:
+            message_perm = ""
+            pass
+
+
+        #message_perm += "\n"
+    #message_perm = message_perm.split("\n")
+    
+    #for mp in message_perm:
+        #print(mp)
+        #cipher_suite = Fernet(key)
+        #plain_text = cipher_suite.decrypt(bytes(perm, encoding='utf-8'))
+        #print("\n"+str(plain_text.decode("utf-8")))
+
+
+def extract():
+    path = "./crack_images/"
+    images = []
+    messages = []
+
+    # Get all images from the specific directory
+    for r, d, i in os.walk(path):
+        for image in i:
+            if '.png' in image:
+                images.append(os.path.join(r, image))
+   
+    # Check all images in the specific directory 
+    for i in images:
+        # Input message to crack
+        img = Image.open(i, 'r')
+        pix = img.load()
+
+        # Pattern to search
+        pattern = re.compile("[-A-Za-z0-9+=_]{14,}")
+        message = ""
+
+        # Print all the pixels of the image
+        for x in range(img.size[0]):
+            for y in range(img.size[1]):
+                r, g, b = pix[x, y]
+
+                # Convert rgb to binary and print it
+                if r % 2 == 1:
+                    r = 1
+                else:
+                    r = 0
+
+                if g % 2 == 1:
+                    g = 1
+                else:
+                    g = 0
+
+                if b % 2 == 1:
+                    b = 1
+                else:
+                    b = 0
+
+                message += str(r) + str(g) + str(b)
+
+        # Convert the binary to ascii
+        message = (int(message, 2).to_bytes((len(message) + 7) // 8, 'big')).decode("latin-1")
+
+        # Match the pattern inside the ascii text
+        matches = re.findall(pattern, str(message))
+        print("\nPossible messages:")
+        for m in matches:
+            messages.append(m)
+            print(m)
+
+    brute_force(messages)
+
+
+def hide():
+    # Input message and converse it to binary
+    word = input("Give message to hide: ")
+    bin_word = (''.join(format(ord(x), '08b') for x in word))
+
+    # Input original image name
+    img = Image.open("./original_images/" + input("Give the name of the original image: "), 'r')
+    pix = img.load()
+
+    # Input the name of the encoded image to export
+    img_output = input("Give the name of the output image: ")
+
+    # Counters for r g b values
+    bit1 = 0
+    bit2 = 1
+    bit3 = 2
+
+    # Iteration through the pixels by row
+    for x in range(400, 401):
+        for y in range(math.ceil(len(bin_word) / 3)):
+            try:
+                r, g, b = pix[x, y]
+
+                # Change the the rgb value to odd and even based on the binary value
+                if bin_word[bit1] == "1":
+                    if r % 2 == 0:
+                        r += 1
+                else:
+                    if r % 2 == 1:
+                        r -= 1
+
+                if bin_word[bit2] == "1":
+                    if g % 2 == 0:
+                        g += 1
+                else:
+                    if g % 2 == 1:
+                        g -= 1
+
+                if bin_word[bit3] == "1":
+                    if b % 2 == 0:
+                        b += 1
+                else:
+                    if b % 2 == 1:
+                        b -= 1
+
+                pix[x, y] = (r, g, b)
+
+                bit1 = bit1 + 3
+                bit2 = bit2 + 3
+                bit3 = bit3 + 3
+            except IndexError:
+                pix[x, y] = (r, g, b)
+                break
+
+    # Save the new image
+    img.save("./encoded_images/" + img_output)
+
+
+def analyze():
+    # Input message and converse it to binary
+    img = Image.open("./crack_images/" + input("Give image name: "), 'r')
+    pix = img.load()
+
+    message = ""
+
+    # Print all the pixels of the image
+    for x in range(img.size[0]):
+        for y in range(img.size[1]):
+            r, g, b = pix[x, y]
+            
+            # Example
+            #if r != 0 or g != 0 or b != 0:
+                #print(r, g, b)
+            
+            # Convert rgb to binary and print it
+            if r % 2 == 1:
+                r = 1
+            else:
+                r = 0
+
+            if g % 2 == 1:
+                g = 1
+            else:
+                g = 0
+
+            if b % 2 == 1:
+                b = 1
+            else:
+                b = 0
+
+            message += str(r) + str(g) + str(b)
+
+    # Write the binary into a file
+    with open("results.txt", 'w') as fp:
+        fp.write(message + '\n')
+
+
+def main():
+    choice = '0'
+
+    # Menu
+    while choice == '0':
+        print("1. Analyze")
+        print("2. Hide")
+        print("3. Extract")
+        print("4. Encrypt")
+        print("5. Decrypt")
+
+        choice = input("")
+        print("")
+
+        if choice == "1":
+            analyze()
+        elif choice == "2":
+            hide()
+        elif choice == "3":
+            extract()
+        elif choice == "4":
+            encryption()
+        elif choice == "5":
+            decryption()
+        else:
+            print("Please make a choice: ")
+    print("\nFinished!")
+
+
+# Calling main function
+main()
